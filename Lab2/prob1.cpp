90ba41f6e33976e0d9f90800c00370058039e885
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2018-03-08 20:44:11 +0800

    Lab1 all done, lab2 done problem1

---
+++ Lab2/prob1.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+//#include <opencv2/imgproc.hpp>
+
+using namespace std;
+using namespace cv;
+
+void histogram_equal(Mat& input, Mat& output);
+
+int main(int argc, char** argv)
+{
+
+    Mat input_img = imread(argv[1]);
+    //since the bgr channel is used for default action, then the BGR 3 channel image must be converted to GREY channel
+    cvtColor(input_img, input_img, CV_BGR2GRAY);
+    Mat output_img = input_img.clone();
+    histogram_equal(input_img, output_img);
+
+    imshow("origin", input_img);
+    imshow("histogram_equal", output_img);
+    waitKey(0);
+
+    imwrite("output_result.jpg", output_img);
+
+  return 0;
+}
+void histogram_equal(Mat& input, Mat& output)
+{
+    vector<int> hash_distribution;
+    vector<double> intensity_cdf;
+    hash_distribution.resize(256);
+    intensity_cdf.resize(256);
+    for(int i=0;i<input.rows;i++)
+    {
+        for(int j=0;j<input.cols;j++)
+        {
+            hash_distribution[(int) input.at<uchar>(i,j)]++;
+        }
+    }
+
+    //search the maxium value
+    int max_value = 0, cnt=0;
+    double cumulative_cnt = 0.0f;
+    for(int i=0;i<hash_distribution.size();i++)
+    {
+        if(hash_distribution[i]!=0)
+        {
+            max_value = max(max_value, i);
+            cumulative_cnt += (double) hash_distribution[i] / (double)(input.rows * input.cols);
+            cnt += hash_distribution[i];
+            intensity_cdf[i] = cumulative_cnt;
+            cout<<"cnt "<<cnt<<" at "<< i<<" Cumulate to "<<cumulative_cnt<<" where intensity is now "<<intensity_cdf[i]<<endl;
+        }
+    }
+    for(int i=0;i<input.rows;i++)
+    {
+        for(int j=0;j<input.cols;j++)
+        {
+            // cout<<" input graph value "
+            // <<(int)input.at<uchar>(i,j)<<" with intensity_cdf "<<intensity_cdf[input.at<uchar>(i,j)]
+            // <<" and output value is "<<intensity_cdf[input.at<uchar>(i,j)] * max_value<<endl;
+            // cout<<" count we have "<<hash_distribution[input.at<uchar>(i,j)]<<endl;
+            output.at<uchar>(i,j) = (intensity_cdf[input.at<uchar>(i,j)] * max_value );
+        }
+    }
+
+}
